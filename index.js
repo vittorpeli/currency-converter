f1ce660ab65f6bf1be5002d7104392ad7714c250
vittor <vittor.pelicarto137@gmail.com>
2023-12-08 15:09:30 -0300

    final update

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 const api = "https://api.exchangerate-api.com/v4/latest/USD"
+// let apiKey = e8671b5ce9a88a863b95867f95ad902e
 
 let search = document.querySelector(".searchBox");
 let convertBtn = document.querySelector(".convertBtn")
-let fromCurrency = document.querySelector(".from")
-let toCurrency = document.querySelector(".to")
+let fromCurrency = document.querySelector("#from")
+let toCurrency = document.querySelector("#to")
 let finalAmount = document.getElementById("finalAmount")
 let finalValue = document.querySelector(".finalValue")
-let resultfrom
+let resultFrom
 let resultTo
 let searchValue
-
-fromCurrency.addEventListener("change", (event) => {
-  resultfrom = `${event.target.value}`
-})
-
-toCurrency.addEventListener("change", (event) => {
-  resultTo = `${event.target.value}`
-})
-
-search.addEventListener ('input', updateValue)
-
-const updateValue = (e) => {
-  searchValue = e.target.value
-}
-
-convertBtn.addEventListener('click',  getResult)
 
 const getResult = () => {
   fetch(`${api}`)
@@ -32,7 +17,25 @@ const getResult = () => {
       return res.json()
     })
     .then(displayResults)
+  console.log(res)
+  console.log(displayResults)
 }
+
+fromCurrency.addEventListener("change", (event) => {
+  resultFrom = `${event.target.value}`
+})
+
+toCurrency.addEventListener("change", (event) => {
+  resultTo = `${event.target.value}`
+})
+
+const updateValue = (e) => {
+  searchValue = e.target.value
+}
+
+search.addEventListener ('input', updateValue)
+
+convertBtn.addEventListener('click',  getResult)
 
 const displayResults = (currency) => {
   let fromRate = currency.rates[resultFrom]
