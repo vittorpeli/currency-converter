b2811f5c3fe20fd26c87541bac290b35c365e34d
vittor <vittor.pelicarto137@gmail.com>
2023-12-08 14:53:22 -0300

    update final

--- index.js
+++ index.js
@@ -4,15 +4,40 @@ let search = document.querySelector(".searchBox");
 let convertBtn = document.querySelector(".convertBtn")
 let fromCurrency = document.querySelector(".from")
 let toCurrency = document.querySelector(".to")
-let finalAmount = document.querySelector(".finalAmount")
+let finalAmount = document.getElementById("finalAmount")
 let finalValue = document.querySelector(".finalValue")
 let resultfrom
+let resultTo
+let searchValue
 
-fetch(api)
-  .then((res) => {
-    console.log(res)
-  })
+fromCurrency.addEventListener("change", (event) => {
+  resultfrom = `${event.target.value}`
+})
 
-// fromCurrency?.addEventListener("change", (event) => {
-//   resultfrom = `${event.target.value}`
-// })
+toCurrency.addEventListener("change", (event) => {
+  resultTo = `${event.target.value}`
+})
+
+search.addEventListener ('input', updateValue)
+
+const updateValue = (e) => {
+  searchValue = e.target.value
+}
+
+convertBtn.addEventListener('click',  getResult)
+
+const getResult = () => {
+  fetch(`${api}`)
+    .then(res => {
+      return res.json()
+    })
+    .then(displayResults)
+}
+
+const displayResults = (currency) => {
+  let fromRate = currency.rates[resultFrom]
+  let toRate = currency.rates[resultTo]
+
+  finalValue.innerHTML = ((toRate/fromRate) * searchValue).toFixed(2)
+  finalAmount.style.display = "block"
+}
